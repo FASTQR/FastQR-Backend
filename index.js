8e83179704053844b6632a825ae96f8576c6bd4e
Ubong Edem <ubongedem78@gmail.com>
2024-01-08 15:21:24 +0100

    Database Config && Packages Added.

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const app = express();
+const { sequelize } = require("./src/config/database_config");
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("I AM WORKING BUT YOUVE GOTTA WORK TOO!");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  sequelize
+    .authenticate()
+    .then(() => {
+      console.log("Database connected");
+    })
+    .catch((error) => {
+      console.log("Error connecting to FASTQR Database", error);
+    });
 });
+
+
